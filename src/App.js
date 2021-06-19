b22a7a6aa4666d34b67ba4dfbba3de4115894579
Dev Chaudhary <ryuk2684@gmail.com>
2021-06-19 18:24:18 +0530

    first commit from linux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from React</h1>
+      <h1>Hello from me too!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
